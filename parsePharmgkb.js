0d516b50c3e8cec47f6e23e2682cee4b329dcd3d
Kevin Dalleau <kevin.dalleau@telecomnancy.eu>
2015-05-20 19:02:06 +0200

    Major bugfix : error, diseases retrieved instead of drugs

--- parsePharmgkb.js
+++ parsePharmgkb.js
@@ -21,8 +21,8 @@ stream.on('data', function(data) {
     var entity2_id = lineToArray[3];
     var entity2_type = lineToArray[5];
     var association = lineToArray[7];
-    if(association=='associated' && ((entity1_type=='Disease' && entity2_type=='Gene')||(entity1_type=='Gene' && entity2_type=='Disease'))) {
-      if(entity1_type=='Gene' && entity2_type=='Disease') {
+    if(association=='associated' && ((entity1_type=='Drug' && entity2_type=='Gene')||(entity1_type=='Gene' && entity2_type=='Drug'))) {
+      if(entity1_type=='Gene' && entity2_type=='Drug') {
         console.log(entity1_id + ";" + entity2_id + ";" + association);
       }
       else {
@@ -47,8 +47,8 @@ stream.on('data', function(data) {
     var entity2_id = lineToArray[3];
     var entity2_type = lineToArray[5];
     var association = lineToArray[7];
-    if(association=='not associated' && ((entity1_type=='Disease' && entity2_type=='Gene')||(entity1_type=='Gene' && entity2_type=='Disease'))) {
-      if(entity1_type=='Gene' && entity2_type=='Disease') {
+    if(association=='not associated' && ((entity1_type=='Drug' && entity2_type=='Gene')||(entity1_type=='Gene' && entity2_type=='Drug'))) {
+      if(entity1_type=='Gene' && entity2_type=='Drug') {
         console.log(entity1_id + ";" + entity2_id + ";" + association);
     }
     else {
@@ -62,6 +62,39 @@ stream.on('end', function (value) {
 })
 }
 
+function isAssociated(gene,drug) {
+  var stream = 	byline(fs.createReadStream('./relationships.tsv'));
+  stream.setEncoding('utf8');
+  var result=false;
+  stream.on('data', function(data) {
+      var lineToArray = data.split("\t");
+      var entity1_id = lineToArray[0];
+      var entity1_type = lineToArray[2];
+      var entity2_id = lineToArray[3];
+      var entity2_type = lineToArray[5];
+      var association = lineToArray[7];
+      console.log("Hello");
+      if(association=='associated' && ((entity1_type=='Drug' && entity2_type=='Gene')||(entity1_type=='Gene' && entity2_type=='Drug'))) {
+        if(entity1_type=='Gene' && entity2_type=='Disease') {
+          if(entity1_id==gene && entity2_id==drug) {
+            result = true;
+
+          }
+        }
+        else {
+          if(entity2_id==gene && entity1_id==drug) {
+            result = true;
+          }
+
+        }
+      }
+    })
+
+    stream.on('end',function() {
+      console.log(result);
+    })
+
+}
 
 program
   .version('0.0.1');
@@ -75,6 +108,17 @@ program.command('getAssociations <param>')
   }
   else if(param== 'negative') {
     getNegativeAssociations();
+  }
+});
+
+program.command('isAssociated <gene> <drug> <param>')
+.description('Get list of associations of a give type')
+.action(function(gene, drug, param){
+  if(param == 'positive') {
+    isAssociated(gene,drug);
+  }
+  else if(param== 'negative') {
+      console.log(gene+drug);
   }
 });
 
